3141af0e592c678ed83ba8ce63b1061ff73dbf9b
Ernest Lu <eluindustries@gmail.com>
2024-01-24 12:51:38 -0800

    updated test-split

--- test_split.cpp
+++ test_split.cpp
@@ -14,23 +14,41 @@ g++ split.cpp test_split.cpp -o test_split
 
 int main(int argc, char* argv[])
 {
-    /*alternatively, write a linked list class*/
-
     Node* one = new Node(14, NULL);
-    Node* two = new Node(15, one);
-    Node* three = new Node(17, two);
-    Node* four = new Node(18, three);
-    Node* five = new Node(20, four);
-    Node* six = new Node(21, five);
-    Node* seven = new Node(1, six);
-    Node* eight = new Node(2, seven);
-    Node* nine = new Node(0, eight);
-    Node* ten = new Node(958, nine);
+
+    Node* head = one;
+
+    Node* two = new Node(15, NULL);
+    one->next = two;
+
+    Node* three = new Node(17, NULL);
+    two->next = three;
+
+    Node* four = new Node(18, NULL);
+    three->next = four;
+
+    Node* five = new Node(20, NULL);
+    four->next = five;
+
+    Node* six = new Node(21, NULL);
+    five->next = six;
+
+    Node* seven = new Node(1, NULL);
+    six->next = seven;
+
+    Node* eight = new Node(2, NULL);
+    seven->next = eight;
+
+    Node* nine = new Node(0, NULL);
+    eight->next = nine;
+
+    Node* ten = new Node(958, NULL);
+    nine->next = ten;
 
     Node* odds = NULL;
     Node* evens = NULL;
 
-    split(ten, odds, evens);
+    split(head, odds, evens);
 
     /*print out the original linked list; prints out the evens; prints out the odds: */
 }
